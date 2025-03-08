d535b4d7a0892b16d2e0373146bc3436dca72ba1
tyroPatel <tyresecook25@yahoo.com>
2025-03-07 21:34:32 -0600

    Fixed Gallery responsive text + Image expansion issue

--- js/carouselk.js
+++ js/carouselk.js
@@ -1,22 +1,6 @@
 
 
-function isCarouselGoing () {
-	return ($('.carousel-item').length > 0) ? true : false;
-}
 
-function handleCarouselK (){ 
-	//window >= sm carousel Still going -> remove class
-	//window >= sm carousel not moving
-	if (window.innerWidth >= 574 && isCarouselGoing()) {
-		$('.killCarousel').removeClass('carousel-item');
-	} else if (window.innerWidth <= 574 && !isCarouselGoing()){
-		$('.killCarousel').addClass('carousel-item');
-	} else {
-
-	}
-}
-$(window).on('resize', handleCarouselK);
-handleCarouselK();
 let activeCategoryTitle = document.querySelector('.galleries-container.active').getAttribute('name');
 
 /*
