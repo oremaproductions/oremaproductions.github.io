e5b43f0538dc8cd3cd39c018623513fdeb8d088f
tyroPatel <tyresecook25@yahoo.com>
2025-08-09 07:29:26 -0500

    Added About page

--- js/global.js
+++ js/global.js
@@ -1,3 +1,19 @@
 function redirectTo(link, method) {
     return (method == 'replace') ? window.location.href = link : window.open(link);
 }
+function toggleCollabContent(){
+    let content = $('#collabContentMore');
+    const trigger = $('#expandCollabContent');
+    console.log(content);
+    if(content.hasClass('d-none')){
+        trigger.text('See Less');
+        trigger.addClass('active');
+        content.removeClass('d-none');
+        content.addClass('d-flex');
+    } else {
+        trigger.text('Learn More');
+        trigger.removeClass('active')
+        content.addClass('d-none');
+        content.removeClass('d-flex');
+    }
+}
